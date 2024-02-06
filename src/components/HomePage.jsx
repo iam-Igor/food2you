90e5a2194414aad78aec3fc497c5c16818c0716a
Ygor Garofalo <igorgarofalo@gmail.com>
2024-02-06 17:33:13 +0100

    added google maps map in modal for selecting address

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -3,13 +3,17 @@ import { Col, Container, Row, Form, Button } from "react-bootstrap";
 import bgHeader from "../assets/img/wave-haikei.svg";
 import hamburger from "../assets/img/95af3cd3-85de-4f17-92e8-647af7967071-removebg-preview.png";
 import { useEffect, useState } from "react";
+import SelectCityModal from "./SelectCityModal";
 
 const Homepage = () => {
   const [formVisible, setFormVisible] = useState(false);
 
+  const [showCityModal, setShowCityModal] = useState(false);
+
   useEffect(() => {
     const handleScroll = () => {
-      if (window.scrollY > 360) {
+      console.log(window.scrollY);
+      if (window.scrollY > 600) {
         setFormVisible(true);
       } else {
         setFormVisible(false);
@@ -20,7 +24,11 @@ const Homepage = () => {
 
   return (
     <Container fluid>
-      <Row className="header-bg d-flex flex-md-row justify-content-center align-items-center">
+      <Row className="header-bg d-flex flex-column flex-md-row justify-content-center align-items-center">
+        <h3 className="text-white text-center w-50">
+          Sapore di consegne, rapidità di servizio: il tuo mondo a portata di
+          clic con Food2You!
+        </h3>
         <Col className=" align-items-center justify-content-around py-5 col-md-4">
           <div className="d-flex">
             <img alt="hamburger" src={hamburger} style={{ width: "100%" }} />
@@ -37,14 +45,13 @@ const Homepage = () => {
                 type="email"
                 placeholder="Es. Napoli"
                 className="rounded-4"
+                onClick={() => {
+                  setShowCityModal(true);
+                }}
               />
             </Form.Group>
-            <Button className="mt-2 rounded-pill align-self-start">
-              Cerca
-            </Button>
           </Form>
         </Col>
-
         <Col className={formVisible ? "col-12 scende mt-3" : "col-12 sale "}>
           <Form className="d-flex  flex-column ">
             <p className="text-white">
@@ -56,23 +63,23 @@ const Homepage = () => {
                 type="email"
                 placeholder="Es. Napoli"
                 className="rounded-4"
+                onClick={() => {
+                  setShowCityModal(true);
+                }}
               />
             </Form.Group>
-            <Button className="mt-2 rounded-pill align-self-start">
-              Cerca
-            </Button>
           </Form>
         </Col>
       </Row>
       <Row className="p-0">
         <img src={bgHeader} className="p-0" />
       </Row>
-      <Row className="p-0">
-        <img src={bgHeader} className="p-0" />
-      </Row>
-      <Row className="p-0">
-        <img src={bgHeader} className="p-0" />
-      </Row>
+      {showCityModal && (
+        <SelectCityModal
+          show={showCityModal}
+          setShow={() => setShowCityModal(false)}
+        />
+      )}
     </Container>
   );
 };
---
+++ src/components/SelectCityModal.jsx
@@ -0,0 +1,101 @@
+import { Button, Col, Form, Modal, Row } from "react-bootstrap";
+import { googleApikey } from "../apifile";
+import { GoogleApiWrapper, Map, Marker } from "google-maps-react";
+import { useEffect, useState } from "react";
+
+const SelectCityModal = ({ show, setShow, google }) => {
+  const [position, setPosition] = useState({ lat: null, lng: null });
+
+  const [address, setAddress] = useState("");
+  const [cityName, setCityName] = useState("");
+
+  const containerStyle = {
+    position: "relative",
+    width: "100",
+    height: "250px",
+  };
+
+  const getData = () => {
+    fetch(
+      "https://maps.googleapis.com/maps/api/geocode/json?address=Via+" +
+        address +
+        "," +
+        cityName +
+        "&key=AIzaSyAObGi76zTUoyYXgesUhxM5TqhxV5KQ3z0"
+    )
+      .then((res) => {
+        if (res.ok) {
+          return res.json();
+        } else {
+          throw new Error("error");
+        }
+      })
+      .then((data) => {
+        console.log(data);
+        setPosition({
+          lat: data.results[0].geometry.location.lat,
+          lng: data.results[0].geometry.location.lng,
+        });
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
+  return (
+    <Modal size="lg" centered show={show}>
+      <div className="text-center pt-3">
+        <h3>Aggiungi il tuo indirizzo di consegna</h3>
+      </div>
+      <hr></hr>
+      <Row className="d-flex flex-column flex-md-row py-3 justify-content-around px-2">
+        <Col className="col-12 col-md-5">
+          <Form.Group className="mb-3 d-flex align-items-center">
+            <Form.Control
+              type="text"
+              placeholder="Es. Via Roma 50"
+              onChange={(e) => {
+                setAddress(e.target.value);
+              }}
+            />
+          </Form.Group>
+          <Form.Group className="mb-3 d-flex align-items-center">
+            <Form.Control
+              type="text"
+              placeholder="Città Es. Milano "
+              onChange={(e) => {
+                setCityName(e.target.value);
+              }}
+            />
+            <i className="bi bi-search ms-1 fs-4" onClick={() => getData()}></i>
+          </Form.Group>
+        </Col>
+        <Col className="col-12 col-md-6">
+          {position.lat !== null && (
+            <Map
+              google={google}
+              zoom={15}
+              containerStyle={containerStyle}
+              initialCenter={{ lat: position.lat, lng: position.lng }}
+            >
+              <Marker
+                position={{ lat: position.lat, lng: position.lng }}
+                icon={{
+                  anchor: new window.google.maps.Point(32, 32),
+                  scaledSize: new window.google.maps.Size(38, 38),
+                }}
+              ></Marker>
+            </Map>
+          )}
+        </Col>
+      </Row>
+      <Modal.Footer>
+        <Button onClick={setShow}>Savese</Button>
+      </Modal.Footer>
+    </Modal>
+  );
+};
+
+export default GoogleApiWrapper({
+  apiKey: googleApikey,
+})(SelectCityModal);
