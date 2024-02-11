382446a3e5155dfbf13c3b92c5f8ef5886560cce
Ygor Garofalo <igorgarofalo@gmail.com>
2024-02-11 17:05:46 +0100

    added POST for making new order, working good

--- src/components/PaymentModal.jsx
+++ src/components/PaymentModal.jsx
@@ -2,10 +2,11 @@ import { useEffect, useState } from "react";
 import { Accordion, Button, Col, Form, Modal } from "react-bootstrap";
 import paymenLogos from "../assets/img/Credit-Card-Icons-removebg-preview.png";
 import { Alert } from "@mui/material";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { googleApikey } from "../apifile";
+import { useNavigate } from "react-router-dom";
 
-const PaymentModal = ({ show, setShow, payload, total }) => {
+const PaymentModal = ({ show, setShow, total }) => {
   const [saved, setSaved] = useState(false);
   const [savedPaymentInfo, setSavedPaymentInfo] = useState(false);
   const [paymentSelected, setPaymentSelected] = useState("");
@@ -17,6 +18,37 @@ const PaymentModal = ({ show, setShow, payload, total }) => {
 
   const [order, setOrder] = useState(false);
   const dispatch = useDispatch();
+
+  const navigate = useNavigate();
+
+  const payloadOrder = useSelector((state) => state.orderPayload);
+
+  console.log(payloadOrder);
+
+  const makeNeworder = () => {
+    fetch("http://localhost:3030/orders/new", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: localStorage.getItem("tokenUser"),
+      },
+      body: JSON.stringify(payloadOrder),
+    })
+      .then((res) => {
+        if (res.ok) {
+          return res.json();
+        } else {
+          throw new Error("errore nel login");
+        }
+      })
+      .then((data) => {
+        console.log(data);
+        alert("ordine okkkkkk");
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
 
   const getData = () => {
     fetch(
@@ -43,6 +75,17 @@ const PaymentModal = ({ show, setShow, payload, total }) => {
           type: "SET_LAT",
           payload: data.results[0].geometry.location.lat,
         });
+
+        dispatch({ type: "SET_ORDER_COMPLETED", payload: true });
+        dispatch({ type: "SHOW_NOTIFICATION", payload: true });
+        setOrder(true);
+        setTimeout(() => {
+          setOrder(false);
+          setShow();
+          dispatch({ type: "SHOW_CART", payload: false });
+          makeNeworder();
+          navigate("/");
+        }, 4000);
       })
       .catch((err) => {
         console.log(err);
@@ -274,25 +317,11 @@ const PaymentModal = ({ show, setShow, payload, total }) => {
           Annulla
         </Button>
         {order ? (
-          <div className="success-animation d-flex justify-content-start">
-            <svg
-              className="checkmark"
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 52 52"
-            >
-              <circle
-                className="checkmark__circle"
-                cx="26"
-                cy="26"
-                r="25"
-                fill="none"
-              />
-              <path
-                className="checkmark__check"
-                fill="none"
-                d="M14.1 27.2l7.1 7.2 16.7-16.8"
-              />
-            </svg>
+          <div className="lds-ellipsis">
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
           </div>
         ) : (
           <Button
@@ -301,7 +330,7 @@ const PaymentModal = ({ show, setShow, payload, total }) => {
               getData();
             }}
           >
-            ordina
+            Ordina
           </Button>
         )}
       </Modal.Footer>
